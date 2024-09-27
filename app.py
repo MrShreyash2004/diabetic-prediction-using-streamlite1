441d9c7be13418ed275dafbe0f6132c0a613c9ab
MrShreyash2004 <mrshreyash08@gmail.com>
2024-09-27 18:53:26 +0530

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split  
 from sklearn.preprocessing import StandardScaler  
 from sklearn.linear_model import LogisticRegression  
-from sklearn.metrics import accuracy_score  
+from sklearn.metrics import accuracy_score 
+import MySQLdb
 import mysql.connector  
 from mysql.connector import Error  
 import hashlib  
